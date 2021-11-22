d2e420f373d62126ed9bf87664e9da910b5ccd04
manubodega <manuel.palomeque@bootcamp-upgrade.com>
2021-11-22 18:47:56 +0100

    iteracion#1

---
+++ iteracion1.js
@@ -0,0 +1,10 @@
+function sum(numberOne, numberTwo){
+    if (numberOne > numberTwo){
+        return (numberOne)
+    }
+    else{
+        return(numberTwo)
+    }
+}
+var a = sum(4,5)
+console.log(a)
